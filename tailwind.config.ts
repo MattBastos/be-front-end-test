c0c25c7aa43596a75ee0bf3144e811b43d9e0309
MattBastos <matthewsbasttos@gmail.com>
2024-06-28 17:08:40 -0300

    style: Add colors to tailwind config file

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,10 +8,14 @@ const config: Config = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      colors: {
+        gradientLightPrimary: '#5A84C0',
+        gradientDartPrimary: '#594Ed2',
+        neutralBlack: '#1C1C1C',
+        neutralWhite: '#FFFFFF',
+        lightGray: '#F0F0F0',
+        midGray: '#DFDFDF',
+        darkGray: '#9E9E9E',
       },
     },
   },
